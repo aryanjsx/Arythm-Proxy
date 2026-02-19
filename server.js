0c86d7b12cc3ba2fde12cf426c638ba43d500250
Aryan Kumar <kumararyan1929@gmail.com>
2026-02-20 01:09:58 +0530

    Enhance /resolve endpoint by adding error handling for yt-dlp and improving URL management for YouTube video extraction

--- server.js
+++ server.js
@@ -39,31 +39,48 @@ app.get("/resolve", async (req, res) => {
   const videoId = req.query.v;
   if (!videoId) return res.sendStatus(400);
 
+  const url = `https://youtube.com/watch?v=${videoId}`;
+
   const ytdlp = spawn("yt-dlp", [
     "-f",
     "bestaudio[ext=m4a]/bestaudio",
     "-g",
     "--no-check-certificates",
-    "--user-agent",
-    "com.google.android.youtube/19.09.37 (Linux; U; Android 13)",
+    "--force-ipv4",
+    "--geo-bypass",
     "--extractor-args",
-    "youtube:player_client=android",
-    `https://youtube.com/watch?v=${videoId}`
+    "youtube:player_client=android,player_skip=configs",
+    "--add-header",
+    "User-Agent:com.google.android.youtube/19.09.37 (Linux; U; Android 13)",
+    url
   ]);
 
   let audioUrl = "";
+  let err = "";
 
   ytdlp.stdout.on("data", (d) => {
     audioUrl += d.toString();
   });
 
+  ytdlp.stderr.on("data", (e) => {
+    err += e.toString();
+  });
+
   ytdlp.on("close", () => {
+
+    if (!audioUrl.trim()) {
+      console.log("yt-dlp ERROR:", err);
+      return res.status(500).json({ error: err });
+    }
+
     res.json({
       url: audioUrl.trim()
     });
+
   });
 
 });
+
 
 app.listen(10000, () => {
   console.log("Audio proxy running...");
