1e8618d6b546f84e92bd622404227da3c70e0714
Aryan Kumar <kumararyan1929@gmail.com>
2026-02-19 23:33:42 +0530

    Refactor audio endpoint to simplify response handling and remove unnecessary headers

--- server.js
+++ server.js
@@ -13,21 +13,20 @@ app.get("/audio", (req, res) => {
   const ytdlp = spawn("yt-dlp", [
     "-f",
     "bestaudio[ext=m4a]/bestaudio",
-    "-o",
-    "-",
-    "--no-playlist",
+    "-g",
     url
   ]);
 
-  res.setHeader("Content-Type", "audio/mp4");
-  res.setHeader("Accept-Ranges", "bytes");
-  res.setHeader("Cache-Control", "no-store");
+  let audioUrl = "";
 
-  ytdlp.stdout.pipe(res);
+  ytdlp.stdout.on("data", (data) => {
+    audioUrl += data.toString();
+  });
 
-  ytdlp.stderr.on("data", d =>
-    console.log("yt-dlp:", d.toString())
-  );
+  ytdlp.on("close", () => {
+    audioUrl = audioUrl.trim();
+    res.redirect(audioUrl);
+  });
 
 });
 
